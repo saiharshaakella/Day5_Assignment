6f67291673af82b3116b2b267f947e63f04ce592
Sai Bhanu Harsha Vardhan Akella <66198044+saiharshaakella@users.noreply.github.com>
2021-07-21 17:24:52 +0530

    Update CardSection.js

--- src/Components/CardSection.js
+++ src/Components/CardSection.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import Card from './Card'
 
-const CardSection = () => {
-  return (
-    <section className="contact bg-success ">
-      <div className="container ">
-        <h2 className="text-white">
-          We love new friends!
-        </h2>
-        <div className="row">
-           <Card cardTitle="Orange" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
-           buttonText="Pear"/>
-           <Card cardTitle="Guava" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
-           buttonText="Apple"/>
-           <Card cardTitle="Mango" cardDescription="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
-           buttonText="Pineapple"/>
+let CardSection = ()=>{
+    return(
+        <section className="contact bg-success ">
+        <div className="container ">
+			<h2 className="text-white">
+				We love new friends!
+			</h2>
+			<div className="row">
+			<Card cardImage= {<img
+                src="https://images.pexels.com/photos/3532557/pexels-photo-3532557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                class="card-img-top"
+                alt="..."
+              />} cardTitle="First" cardDesc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui corrupti debitis, ipsum possimus aperiam asperiores. Fuga suscipit odio voluptas cum quod sequi commodi ad totam explicabo dolorum, ea, dignissimos velit?"
+			buttontxt="Market"/> 
+			<Card cardImage={<img
+                src="https://images.pexels.com/photos/3532544/pexels-photo-3532544.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                class="card-img-top"
+                alt="..."
+              />} cardTitle="Second" cardDesc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui corrupti debitis, ipsum possimus aperiam asperiores. Fuga suscipit odio voluptas cum quod sequi commodi ad totam explicabo dolorum, ea, dignissimos velit?"
+			buttontxt="Station"/>
+			<Card cardImage={ <img
+                src="https://images.pexels.com/photos/2522663/pexels-photo-2522663.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                class="card-img-top"
+                alt="..."
+              />} cardTitle="Third" cardDesc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui corrupti debitis, ipsum possimus aperiam asperiores. Fuga suscipit odio voluptas cum quod sequi commodi ad totam explicabo dolorum, ea, dignissimos velit?"
+			buttontxt="City"/>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
 }
+
 export default CardSection
